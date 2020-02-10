75f778819830b8eb6e4e34b7754bb09be1301af7
ddhorstman <davidzilla27@gmail.com>
2020-02-09 22:11:12 -0500

    Stretch: Prev/Next buttons.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,33 +2,63 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from "axios";
 import StarWarsCard from "./components/StarWarsCard";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Button, ButtonGroup } from "reactstrap";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters, setCharacters] = useState([{ "name": "Luke" }]);
+  const [characters, setCharacters] = useState([{}]);
+  const [url, setUrl] = useState({
+    next: null,
+    current: "https://swapi.co/api/people/",
+    previous: null
+  });
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(() => {
     axios
-      .get("https://swapi.co/api/people/?format=json")
+      .get(url.current)
       .then(r => {
-        console.log(r.data);
+        //console.log(r.data);
+        setUrl({
+          next: r.data.next,
+          current: url.current,
+          previous: r.data.previous
+        });
         setCharacters(r.data.results);
       })
       .catch(console.error)
   }
 
-    , []);
+    , [url.current]);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <ButtonGroup className="mb-4">
+        <Button
+          disabled={url.previous === null}
+          onClick={() => setUrl({
+            next: url.current,
+            current: url.previous,
+            previous: null
+          })}>
+          Previous
+      </Button>
+        <Button
+          disabled={url.next === null}
+          onClick={() => setUrl({
+            next: null,
+            current: url.next,
+            previous: url.current
+          })}>
+          Next
+      </Button>
+      </ButtonGroup>
       <Container>
         <Row>
-          <Col xs="auto" style={{padding:"0px"}}></Col>
+          <Col xs="auto" style={{ padding: "0px" }}></Col>
           {characters.map((person, idx) => <StarWarsCard key={idx} data={person} />)}
         </Row>
       </Container>
--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -2,27 +2,23 @@ import React from "react";
 import "./StarWarsCard.css";
 import { Card, CardBody, CardHeader, CardText, CardTitle } from "reactstrap";
 export default function StarWarsCard(props) {
-    function format(value){
-        value = value.slice(0,1).toUpperCase()+value.slice(1);
-        return value==='N/a' ? "None" : value;
+    function format(value) {
+        value = value.slice(0, 1).toUpperCase() + value.slice(1);
+        return value === 'N/a' ? "None" : value;
     }
     const { name, height, mass, hair_color, skin_color, eye_color, birth_year, gender, homeworld } = props.data;
-    if(name==="Luke")return <Card />;
+    if (!name) return <Card />;
     return (
-    <Card className="person-card mx-auto mb-2">
-        <CardHeader><CardTitle>{name}</CardTitle></CardHeader>
-        <CardBody>
-            <CardText>
-                <ul>
-                    <li>Gender: {format(gender)}</li>
-                    <li>Height: {format(height)}cm</li>
-                    <li>Weight: {format(mass)}kg</li>
-                    <li>Hair Color: {format(hair_color)}</li>
-                    <li>Skin Color: {format(skin_color)}</li>
-                    <li>Eye Color: {format(eye_color)}</li>
-                    <li>Born in {format(birth_year)}</li>
-                </ul>
-            </CardText>
-        </CardBody>
-    </Card>);
+        <Card className="person-card mx-auto mb-4">
+            <CardHeader><CardTitle tag="h2">{name}</CardTitle></CardHeader>
+            <CardBody>
+                <CardText>Gender: {format(gender)}</CardText>
+                <CardText>Height: {format(height)}cm</CardText>
+                <CardText>Weight: {format(mass)}kg</CardText>
+                <CardText>Hair Color: {format(hair_color)}</CardText>
+                <CardText>Skin Color: {format(skin_color)}</CardText>
+                <CardText>Eye Color: {format(eye_color)}</CardText>
+                <CardText>Born in {format(birth_year)}</CardText>
+            </CardBody >
+        </Card >);
 }
